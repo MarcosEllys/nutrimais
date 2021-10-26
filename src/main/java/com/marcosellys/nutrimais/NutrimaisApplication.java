20717d725259322af37cbc43eaaf5aa269ab32d6
MarcosEllys <marcos.ellys@gmail.com>
2021-10-25 22:06:36 -0300

    initial config

---
+++ src/main/java/com/marcosellys/nutrimais/NutrimaisApplication.java
@@ -0,0 +1,13 @@
+package com.marcosellys.nutrimais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrimaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrimaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcosellys/nutrimais/NutrimaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosellys.nutrimais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrimaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
